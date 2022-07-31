fee40affb01114b4348b14764a6eab7ad436f69a
Waaman <github@waaman.fr>
2022-07-31 11:29:25 +0200

    v0.0.4 - Tentative d'utilisation de fonctions.sh

---
+++ fonctions.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+UIDExists(){
+    if id ${1} > /dev/null 2>&1; then
+        echo 1
+    else   
+        echo 0
+    fi
+}
+
+GroupExists(){
+    if getent group ${1} > /dev/null 2>&1; then
+        echo 1
+    else   
+        echo 0
+    fi
+}
+
+PortInUse(){
+    if netstat -tulpn | grep :${1} > /dev/null 2>&1; then
+        echo 1
+    else   
+        echo 0
+    fi
+}
--- scripts/00-create-user-sudo.sh
+++ scripts/00-create-user-sudo.sh
@@ -6,11 +6,12 @@ echo "--------------------------------------------------------"
 
 
 ROOT_DIR=$(dirname $(readlink -f $0))/..
+source ${ROOT_DIR}/fonctions.sh
 
 echo "Nom de l'utilisateur à créer: "
 read user_name
 # Vérifications de saisie
-while id "${user_name}" &>/dev/null;
+while id ${user_name} &>/dev/null;
 do
     echo "Cet utilisateur existe déja."
     echo "Nom de l'utilisateur à créer: (Ctrl+c pour annuler)"
@@ -20,7 +21,8 @@ done
 echo "Son id: (vide = defaut: 99)"
 read user_id
 # Vérifications de saisie
-while getent passwd ${user_id} >/dev/null;
+#while getent passwd ${user_id} >/dev/null;
+while UIDExists ${user_id}
 do
     echo "L'id ${user_id} est déja pris."
     echo "Son id: (vide = defaut: 99) (Ctrl+c pour annuler)"
@@ -31,7 +33,8 @@ done
 echo "Son groupe: (vide = defaut: users)"
 read user_group
 # Vérifications de saisie
-while ! getent group ${user_group} >/dev/null;
+#while ! getent group ${user_group} >/dev/null;
+while ! GroupExists ${user_group}
 do
     echo "Le groupe ${user_group} n'existe pas."
     echo "Son groupe: (vide = defaut: users) (Ctrl+c pour annuler)"
@@ -39,16 +42,16 @@ do
 done
 
 
-if [[ -z "${user_id}" ]] && [[ -z "${user_group}" ]]
+if [[ -z ${user_id} ]] && [[ -z ${user_group} ]]
     then
         user_id=99
         user_group=users
         adduser -u 99 -G users ${user_name} 
-    elif  [[ -z "${user_id}" ]]
+    elif  [[ -z ${user_id} ]]
     then
         user_id=99
         adduser -u ${user_id} -G ${user_group} ${user_name}         
-    elif  [[ -z "${user_group}" ]]
+    elif  [[ -z ${user_group} ]]
     then
         user_group=users
         adduser -u ${user_id} -G ${user_group} ${user_name} 
@@ -61,12 +64,12 @@ echo "Ajout de l'utilisateur dans le groupe wheel"
 adduser ${user_name} wheel
 
 # Stockage de variable dans des fichiers individuels pour utilisation ultèrieure si besoin
-echo ${user_id} >> $ROOT_DIR/user_id
-echo ${user_group} >> $ROOT_DIR/user_group
-echo ${user_name} >> $ROOT_DIR/user_name
+echo ${user_id} >> ${ROOT_DIR}/user_id
+echo ${user_group} >> ${ROOT_DIR}/user_group
+echo ${user_name} >> ${ROOT_DIR}/user_name
 
 
-echo "Votre utilisateur avec les droits sudo est: ${user_name}" >> $ROOT_DIR/motd
-echo "    Son home est /home/${user_name}" >> $ROOT_DIR/motd
-printf "\n" >> $ROOT_DIR/motd
+echo "Votre utilisateur avec les droits sudo est: ${user_name}" >> ${ROOT_DIR}/motd
+echo "    Son home est /home/${user_name}" >> ${ROOT_DIR}/motd
+printf "\n" >> ${ROOT_DIR}/motd
 
